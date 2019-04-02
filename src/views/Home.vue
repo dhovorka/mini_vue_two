d6204284a0e9ce52791c7103c9b28081409e7ba1
Daniel Hovorka <dhovorka10@gmail.com>
2019-04-01 21:27:39 -0500

    added create,show,update,destroy

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,29 @@
 <template>
   <div class="home">
+    <h1>New Product</h1>
+    <div>
+      Name: <input type="text" v-model="newProductName" /> Price:
+      <input type="text" v-model="newProductPrice" /> Description: <input type="text" v-model="newProductDescription" />
+      <button v-on:click="createProduct()">Create Product</button>
+    </div>
     <h1>All Products</h1>
     <div v-for="product in products">
       <h2>{{ product.name }}</h2>
       <img v-bind:src="product.url" />
-      <p>Name: {{ product.name }}</p>
-      <p>Price: {{ product.price }}</p>
-      <!--  <img alt="Vue logo" src="../assets/logo.png" /> -->
-      <HelloWorld msg="Welcome to Your Vue.js App" />
+      <button v-on:click="showProduct(product)">Show More Info</button>
+      <div v-if="currentProduct === product">
+        <p>Name: {{ product.name }}</p>
+        <p>Price: {{ product.price }}</p>
+        <p>Description: {{ product.description }}</p>
+        <div>
+          Name: <input type="text" v-model="product.name" /> Price:
+          <input type="text" v-model="product.price" /> Description: <input type="text" v-model="product.description" />
+          <button v-on:click="updateProduct(product)">Update Product</button>
+          <button v-on:click="destroyProduct(product)">Destroy Product</button>
+        </div>
+        <!--  <img alt="Vue logo" src="../assets/logo.png" /> -->
+        <!--  <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+      </div>
     </div>
   </div>
 </template>
@@ -20,7 +36,11 @@ import axios from "axios";
 export default {
   data: function() {
     return {
-      products: []
+      products: [],
+      currentProduct: {},
+      newProductName: "",
+      newProductPrice: "",
+      newProductDescription: ""
     };
   },
   created: function() {
@@ -28,6 +48,43 @@ export default {
       this.products = response.data;
     });
   },
-  methods: {}
+  methods: {
+    createProduct: function() {
+      var params = {
+        name: this.newProductName,
+        price: this.newProductPrice,
+        description: this.newProductDescription
+      };
+      axios.post("/api/products", params).then(response => {
+        this.products.push(response.data);
+        this.newProductName = "";
+        this.newProductPrice = "";
+        this.newProductDescription = "";
+      });
+    },
+    showProduct: function(product) {
+      if (this.currentProduct === product) {
+        this.currentProduct = {};
+      } else {
+        this.currentProduct = product;
+      }
+    },
+    updateProduct: function(product) {
+      var params = {
+        name: product.name,
+        price: product.price,
+        description: product.description
+      };
+      axios.patch("/api/products/" + product.id, params).then(response => {
+        this.currentProduct = {};
+      });
+    },
+    destroyProduct: function(product) {
+      axios.delete("/api/products/" + product.id).then(response => {
+        var index = this.products.indexOf(product);
+        this.products.splice(index, 1);
+      });
+    }
+  }
 };
 </script>
